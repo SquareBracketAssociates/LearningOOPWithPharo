9a9f3b04f2fb7ac47e2254253aa48133ba648cee
Damien Pollet <damien.pollet@gmail.com>
2017-05-22 15:08:05 +0200

    Use TEXINPUTS for lookup of LaTeX stuff
    Should help simplify the build…

--- support/makefiles/pdf.sub.mk
+++ support/makefiles/pdf.sub.mk
@@ -1,6 +1,7 @@
 -include $(wildcard *.d)
 
 %.pdf: %.tex
+	TEXINPUTS=.:root/support/latex//: \
 	latexmk -lualatex -use-make \
 		-latexoption="--file-line-error --halt-on-error" \
 		-deps-out="$*.d" \
