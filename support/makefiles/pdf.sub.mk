391e70f8503bcf7d082e8a03431ff989a2b6cb8a
Damien Pollet <damien.pollet@gmail.com>
2017-05-30 18:36:56 +0200

    Monkeying around with build system…

--- support/makefiles/pdf.sub.mk
+++ support/makefiles/pdf.sub.mk
@@ -1,8 +1,11 @@
 -include $(wildcard *.d)
 
+%.pdf: root/support/latex/%.pdf
+	ln $<
+
 %.pdf: %.tex
-	TEXINPUTS=.:root/support/latex//: \
-	latexmk -lualatex -use-make \
+	TEXINPUTS=root/support/latex//: \
+	latexmk -lualatex -use-make -recorder \
 		-latexoption="--file-line-error --halt-on-error" \
 		-deps-out="$*.d" \
 		$<
